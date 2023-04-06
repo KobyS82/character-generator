3fcb1f71844cd4e4fb5d78136f0d41e410cc10d9
Lance_Lake <temp@MostDeadlyGame.com>
2023-04-06 19:25:55 -0400

    Updated resolvers.js with user code.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { Character } = require('../models');
+const { Character, User } = require('../models');
 
 const resolvers = {
   Query: {
@@ -7,6 +7,9 @@ const resolvers = {
     },
     getCharacters: async (_, { characterName }, { Character }) => {
       return await Character.find({ characterName });
+    },
+    getUser: async (_, { _id }, { User }) => {
+      return await User.findById(_id);
     },
   },
   Mutation: {
@@ -19,6 +22,30 @@ const resolvers = {
     },
     deleteCharacter: async (_, { _id }, { Character }) => {
       return await Character.findByIdAndRemove(_id);
+    },
+    createUser: async (_, args, { User }) => {
+      const user = await User.create(args);
+      return user;
+    },
+    updateUser: async (_, { _id, ...rest }, { User }) => {
+      return await User.findByIdAndUpdate(_id, rest, { new: true });
+    },
+    deleteUser: async (_, { _id }, { User }) => {
+      return await User.findByIdAndRemove(_id);
+    },
+    login: async (_, { userName, password }, { User }) => {
+      const user = await User.findOne({ userName, password });
+      if (!user) {
+        throw new Error('Invalid credentials');
+      }
+      return user;
+    },
+    logout: async (_, { userName, password }, { User }) => {
+      const user = await User.findOne({ userName, password });
+      if (!user) {
+        throw new Error('Invalid credentials');
+      }
+      return user;
     },
   },
 };
