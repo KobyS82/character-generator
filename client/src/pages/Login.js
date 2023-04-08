344a6c7abe64a9251802370ed516b3922a566443
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-04-08 13:12:53 -0500

    names were fixed

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -19,29 +19,29 @@ const Login = () => {
             <form>
               {/* <!--Sign in section--> */}
               <div className="flex flex-row items-center justify-center lg:justify-start">
-                <p className="mb-0 mr-4 text-lg">Sign in</p>
+                <p className="mb-0 text-lg font-bold mt-3 flex justify-items-center">Sign in</p>
               </div>
 
-              {/* <!-- Separator between social media sign in and email/password sign in --> */}
+              {/* <!-- Separator between social media sign in and username/password sign in --> */}
               <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
                 <p className="mx-4 mb-0 text-center font-semibold dark:text-white">
                   Or
                 </p>
               </div>
 
-              {/* <!-- Email input --> */}
+              {/* <!-- Username input --> */}
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <input
                   type="text"
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleFormControlInput2"
-                  placeholder="Email address"
+                  className="peer block min-h-[auto] w-full rounded bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  id="sign-in-username"
+                  placeholder="Username"
                 />
                 <label
                   for="exampleFormControlInput2"
                   className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
                 >
-                  Email address
+                  Username
                 </label>
               </div>
 
@@ -50,7 +50,7 @@ const Login = () => {
                 <input
                   type="password"
                   className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleFormControlInput22"
+                  id="sign-in-password"
                   placeholder="Password"
                 />
                 <label
@@ -75,29 +75,29 @@ const Login = () => {
             <form>
               {/* <!--Register section--> */}
               <div className="flex flex-row items-center justify-center lg:justify-start">
-                <p className="mb-0 mr-4 text-lg">Register</p>
+                <p className="mb-0 text-lg font-bold mt-3 flex justify-items-center">Register</p>
               </div>
 
-              {/* <!-- Separator between social media sign in and email/password sign in --> */}
+              {/* <!-- Separator between social media sign in and username/password sign in --> */}
               <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
                 <p className="mx-4 mb-0 text-center font-semibold dark:text-white">
                   Or
                 </p>
               </div>
 
-              {/* <!-- Email input --> */}
+              {/* <!-- Username input --> */}
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <input
                   type="text"
                   className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleFormControlInput2"
-                  placeholder="Email address"
+                  id="regester-username"
+                  placeholder="Username"
                 />
                 <label
                   for="exampleFormControlInput2"
                   className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
                 >
-                  Email address
+                  Username
                 </label>
               </div>
 
@@ -106,7 +106,7 @@ const Login = () => {
                 <input
                   type="password"
                   className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleFormControlInput22"
+                  id="regester-password"
                   placeholder="Password"
                 />
                 <label
