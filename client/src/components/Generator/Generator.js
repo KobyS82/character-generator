e9fff70b1a84f1620b7e30355fb521d1aa1d5e57
Caleb Davidson <calebgdavidsonn@gmail.com>
2023-04-08 14:00:10 -0500

    generator dice button

--- client/src/components/Generator/Generator.js
+++ client/src/components/Generator/Generator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useEffect, useState} from 'react'
+import { FaDiceOne, FaDiceTwo, FaDiceThree, FaDiceFour, FaDiceFive, FaDiceSix } from "react-icons/fa";
 
 const Generator = () => {
 
@@ -10,77 +11,83 @@ const Generator = () => {
   let [stat4, setStat4] = useState(0);
   let [stat5, setStat5] = useState(0);
 
-
 return (
 
-<div className="columns-6 flex flex-row w-full justify-space-between">
+<div className="columns-7 flex flex-row w-full justify-space-between border-black">
 
-    <div className="w-40 h-20 bg-black rounded my-6">
-      <button class="text-white bg-transparent font-semibold py-2 px-4 hover:border-transparent rounded text-center mx-4"
+  <div className="w-40 h-[100px] bg-black rounded my-6">
+    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center">Strength</p>
+      <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
+        <p className="text-white text-center self-center">
+        {stat}</p>
+      </div>
+      <button className="absolute flex items-center justify-center mx-[48px]"
       onClick={() => setStat(Math.floor(Math.random() * 16) + 3)}>
-      Strength
-      </button>
-        <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
-          <p className="text-white text-center self-center">
-          {stat}</p>
-        </div>
-      </div>
+    <FaDiceOne size={64} />
+    </button>
+    </div>
 
-      <div className="w-40 h-20 bg-black rounded my-6">
-      <button class="text-white bg-transparent font-semibold py-2 px-4 hover:border-transparent rounded text-center mx-4"
+    <div className="w-40 h-[100px] bg-black rounded my-6">
+    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center">Strength</p>
+      <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
+        <p className="text-white text-center self-center">
+        {stat1}</p>
+      </div>
+      <button className="absolute flex items-center justify-center mx-[48px]"
       onClick={() => setStat1(Math.floor(Math.random() * 16) + 3)}>
-      Dexterity
-      </button>
-        <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
-          <p className="text-white text-center self-center">
-          {stat1}</p>
-        </div>
-      </div>
+    <FaDiceTwo size={64} />
+    </button>
+    </div>
 
-      <div className="w-40 h-20 bg-black rounded my-6">
-      <button class="text-white bg-transparent font-semibold py-2 px-4 hover:border-transparent rounded text-center mx-2"
+    <div className="w-40 h-[100px] bg-black rounded my-6">
+    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center">Strength</p>
+      <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
+        <p className="text-white text-center self-center">
+        {stat2}</p>
+      </div>
+      <button className="absolute flex items-center justify-center mx-[48px]"
       onClick={() => setStat2(Math.floor(Math.random() * 16) + 3)}>
-      Constitution
-      </button>
-        <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
-          <p className="text-white text-center self-center">
-          {stat2}</p>
-        </div>
-      </div>
+    <FaDiceThree size={64} />
+    </button>
+    </div>
 
-      <div className="w-40 h-20 bg-black rounded my-6">
-      <button class="text-white bg-transparent font-semibold py-2 px-4 hover:border-transparent rounded text-center mx-3"
+    <div className="w-40 h-[100px] bg-black rounded my-6">
+    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center">Strength</p>
+      <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
+        <p className="text-white text-center self-center">
+        {stat3}</p>
+      </div>
+      <button className="absolute flex items-center justify-center mx-[48px]"
       onClick={() => setStat3(Math.floor(Math.random() * 16) + 3)}>
-      Intelligence
-      </button>
-        <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
-          <p className="text-white text-center self-center">
-          {stat3}</p>
-        </div>
-      </div>
+    <FaDiceFour size={64} />
+    </button>
+    </div>
 
-      <div className="w-40 h-20 bg-black rounded my-6">
-      <button class="text-white bg-transparent font-semibold py-2 px-4 hover:border-transparent rounded text-center mx-5"
+    <div className="w-40 h-[100px] bg-black rounded my-6">
+    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center">Strength</p>
+      <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
+        <p className="text-white text-center self-center">
+        {stat4}</p>
+      </div>
+      <button className="absolute flex items-center justify-center mx-[48px]"
       onClick={() => setStat4(Math.floor(Math.random() * 16) + 3)}>
-      Wisdom
-      </button>
-        <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
-          <p className="text-white text-center self-center">
-          {stat4}</p>
-        </div>
-      </div>
+    <FaDiceFive size={64} />
+    </button>
+    </div>
 
-      <div className="w-40 h-20 bg-black rounded my-6">
-      <button class="text-white bg-transparent font-semibold py-2 px-4 hover:border-transparent rounded text-center mx-4"
-      onClick={() => setStat5(Math.floor(Math.random() * 16) + 3)}>
-      Charisma
-      </button>
-        <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
-          <p className="text-white text-center self-center">
-          {stat5}</p>
-        </div>
+    <div className="w-40 h-[100px] bg-black rounded my-6">
+    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center">Strength</p>
+      <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
+        <p className="text-white text-center self-center">
+        {stat5}</p>
       </div>
-   
+      <button className="absolute flex items-center justify-center mx-[48px]"
+      onClick={() => setStat5(Math.floor(Math.random() * 16) + 3)}>
+    <FaDiceSix size={64} />
+    </button>
+    </div>
+
+
 </div>
 
   );
