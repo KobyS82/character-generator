b485953ffb673f577afedd80506af052e0b4feb3
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-04-11 18:50:40 -0500

    formatted html files

--- client/src/components/ClassGenerator/ClassGenerator.js
+++ client/src/components/ClassGenerator/ClassGenerator.js
@@ -1,28 +1,53 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import { FaDiceD20 } from "react-icons/fa";
 
 const ClassGenerator = () => {
-   const characterClass = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard','Artificer'];
-   let [stat, setStat] = useState(0);
+  const characterClass = [
+    "Barbarian",
+    "Bard",
+    "Cleric",
+    "Druid",
+    "Fighter",
+    "Monk",
+    "Paladin",
+    "Ranger",
+    "Rogue",
+    "Sorcerer",
+    "Warlock",
+    "Wizard",
+    "Artificer",
+  ];
+  let [stat, setStat] = useState(0);
 
   return (
-<div className="py-[50px] flex flex-row w-full justify-center">
+    <div className="py-[50px] flex flex-row w-full justify-center">
       <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
-      <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">Class</p>
-         <div className="h-20 w-40 rounded-full bg-orange-300 border-white flex justify-center items-center">
-            <p className="text-white text-center self-center text-[30px]">
-            {stat}</p>
-         </div>
-         <div className="mb-20 py-[2px]">
-            <button className="absolute flex items-center justify-center mx-[48px] hover:text-orange-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
-            onClick={() => setStat(characterClass[Math.floor(Math.random() * characterClass.length)])}>
+        <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">
+          Class
+        </p>
+        <div className="h-20 w-40 rounded-full bg-orange-300 border-white flex justify-center items-center">
+          <p className="text-white text-center self-center text-[30px]">
+            {stat}
+          </p>
+        </div>
+        <div className="mb-20 py-[2px]">
+          <button
+            className="absolute flex items-center justify-center mx-[48px] hover:text-orange-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
+            onClick={() =>
+              setStat(
+                characterClass[
+                  Math.floor(Math.random() * characterClass.length)
+                ]
+              )
+            }
+          >
             <FaDiceD20 size={64} />
-            </button>
-         </div>
+          </button>
+        </div>
       </div>
-</div>
-  )
-}
+    </div>
+  );
+};
 
-export default ClassGenerator
+export default ClassGenerator;
--- client/src/components/Generator/Generator.js
+++ client/src/components/Generator/Generator.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { useState } from 'react'
-import { FaDiceOne, FaDiceTwo, FaDiceThree, FaDiceFour, FaDiceFive, FaDiceSix } from "react-icons/fa";
+import React from "react";
+import { useState } from "react";
+import {
+  FaDiceOne,
+  FaDiceTwo,
+  FaDiceThree,
+  FaDiceFour,
+  FaDiceFive,
+  FaDiceSix,
+} from "react-icons/fa";
 
 const Generator = () => {
-
   let [stat, setStat] = useState(0);
   let [stat1, setStat1] = useState(0);
   let [stat2, setStat2] = useState(0);
@@ -11,87 +17,112 @@ const Generator = () => {
   let [stat4, setStat4] = useState(0);
   let [stat5, setStat5] = useState(0);
 
-return (
-
-<div className="columns-6 flex flex-row w-full justify-between border-black justify-around">
-
-  <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
-    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">Strength</p>
-      <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
-        <p className="text-white text-center self-center text-[30px]">
-        {stat}</p>
+  return (
+    <div className="columns-6 flex flex-row w-full justify-between border-black justify-around">
+      <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
+        <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">
+          Strength
+        </p>
+        <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
+          <p className="text-white text-center self-center text-[30px]">
+            {stat}
+          </p>
+        </div>
+        <div className="mb-20">
+          <button
+            className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
+            onClick={() => setStat(Math.floor(Math.random() * 16) + 3)}
+          >
+            <FaDiceOne size={64} />
+          </button>
+        </div>
       </div>
-      <div className="mb-20">
-      <button className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
-      onClick={() => setStat(Math.floor(Math.random() * 16) + 3)}>
-    <FaDiceOne size={64} />
-    </button>
-    </div>
-    </div>
 
-    <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
-    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">Dexterity</p>
-      <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
-        <p className="text-white text-center self-center text-[30px]">
-        {stat1}</p>
+      <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
+        <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">
+          Dexterity
+        </p>
+        <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
+          <p className="text-white text-center self-center text-[30px]">
+            {stat1}
+          </p>
+        </div>
+        <button
+          className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
+          onClick={() => setStat1(Math.floor(Math.random() * 16) + 3)}
+        >
+          <FaDiceTwo size={64} />
+        </button>
       </div>
-      <button className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
-      onClick={() => setStat1(Math.floor(Math.random() * 16) + 3)}>
-    <FaDiceTwo size={64} />
-    </button>
-    </div>
 
-    <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
-    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">Constitution</p>
-      <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
-        <p className="text-white text-center self-center text-[30px]">
-        {stat2}</p>
+      <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
+        <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">
+          Constitution
+        </p>
+        <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
+          <p className="text-white text-center self-center text-[30px]">
+            {stat2}
+          </p>
+        </div>
+        <button
+          className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
+          onClick={() => setStat2(Math.floor(Math.random() * 16) + 3)}
+        >
+          <FaDiceThree size={64} />
+        </button>
       </div>
-      <button className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
-      onClick={() => setStat2(Math.floor(Math.random() * 16) + 3)}>
-    <FaDiceThree size={64} />
-    </button>
-    </div>
 
-    <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
-    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">Intelligence</p>
-      <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
-        <p className="text-white text-center self-center text-[30px]">
-        {stat3}</p>
+      <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
+        <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">
+          Intelligence
+        </p>
+        <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
+          <p className="text-white text-center self-center text-[30px]">
+            {stat3}
+          </p>
+        </div>
+        <button
+          className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
+          onClick={() => setStat3(Math.floor(Math.random() * 16) + 3)}
+        >
+          <FaDiceFour size={64} />
+        </button>
       </div>
-      <button className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
-      onClick={() => setStat3(Math.floor(Math.random() * 16) + 3)}>
-    <FaDiceFour size={64} />
-    </button>
-    </div>
 
-    <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
-    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">Wisdom</p>
-      <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
-        <p className="text-white text-center self-center text-[30px]">
-        {stat4}</p>
+      <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
+        <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">
+          Wisdom
+        </p>
+        <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
+          <p className="text-white text-center self-center text-[30px]">
+            {stat4}
+          </p>
+        </div>
+        <button
+          className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
+          onClick={() => setStat4(Math.floor(Math.random() * 16) + 3)}
+        >
+          <FaDiceFive size={64} />
+        </button>
       </div>
-      <button className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
-      onClick={() => setStat4(Math.floor(Math.random() * 16) + 3)}>
-    <FaDiceFive size={64} />
-    </button>
-    </div>
 
-    <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
-    <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">Charisma</p>
-      <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
-        <p className="text-white text-center self-center text-[30px]">
-        {stat5}</p>
+      <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
+        <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">
+          Charisma
+        </p>
+        <div className="h-20 w-40 rounded-full bg-green-300 flex justify-center items-center">
+          <p className="text-white text-center self-center text-[30px]">
+            {stat5}
+          </p>
+        </div>
+        <button
+          className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
+          onClick={() => setStat5(Math.floor(Math.random() * 16) + 3)}
+        >
+          <FaDiceSix size={64} />
+        </button>
       </div>
-      <button className="absolute flex items-center justify-center mx-[48px] hover:text-green-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
-      onClick={() => setStat5(Math.floor(Math.random() * 16) + 3)}>
-    <FaDiceSix size={64} />
-    </button>
     </div>
-
-
-</div>
-
   );
 };
 
