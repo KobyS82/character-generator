76637bb816858ae2e4b754376448435ceae93245
Jesus <perez.jesus1039@gmail.com>
2023-04-10 19:11:06 -0700

    pulled but not allowing me to work, just doing this to see if it works

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -9,14 +9,9 @@ const Navbar = () => {
             <div className='flex justify-between items-center mt-2 md:mt-0'>
                 <Link to="/about" className="underline text-white hover:text-blue-600 mr-4">About Us</Link>
                 <Link to="/creators" className="underline text-white hover:text-blue-600 mr-4">Creators</Link>
-<<<<<<< HEAD
-                <Link to="/game" className="underline text-white hover:text-blue-600 mr-4">Mini Game</Link>
-                <Link to="/contact" className="underline text-white hover:text-blue-600">Contact</Link>
-=======
                 <Link to="/minigame" className="underline text-white hover:text-blue-600 mr-4">Mini Game</Link>
                 <Link to="/contact" className="underline text-white hover:text-blue-600 mr-4">Contact</Link>
                 <Link to="/login" className="underline text-white hover:text-blue-600">Login</Link>
->>>>>>> ebbd29ebe61bb83d002af53f55df1ce4bc4d1b87
             </div>
         </nav>
     )
