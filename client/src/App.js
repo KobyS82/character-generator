9cfca47fdfed5eb62cbfc0c5b4e69f5375fa63a2
Caleb Davidson <calebgdavidsonn@gmail.com>
2023-04-10 21:03:44 -0500

    updated styling for generators

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
-          <div className="container">
+          <div className="">
             <Routes>
               <Route 
                 path="/" 
--- client/src/components/ClassGenerator/ClassGenerator.js
+++ client/src/components/ClassGenerator/ClassGenerator.js
@@ -1,37 +1,29 @@
 import React from 'react'
 import { useState } from 'react';
-import { FaDiceOne, FaDiceTwo, FaDiceThree, FaDiceFour, FaDiceFive, FaDiceSix } from "react-icons/fa";
+import { FaDiceD20 } from "react-icons/fa";
 
 const ClassGenerator = () => {
    const characterClass = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard','Artificer'];
    let randomValue = characterClass[Math.floor(Math.random() * characterClass.length)];
    let position = characterClass.indexOf(randomValue);
    let [stat, setStat] = useState(0);
-   
-   //console.log(randomValue);
-   //console.log(position);
-   //console.log('jw');
 
   return (
-  <div className="columns-7 flex flex-row w-full justify-space-between border-black">
-      <div className="w-40 h-[100px] bg-black rounded my-6">
-      <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">Classssssssssssss</p>
-         <div className="h-20 w-40 rounded-full bg-black border border-white flex justify-center items-center">
+<div className="py-[50px] flex flex-row w-full justify-center">
+      <div className="w-40 h-[90px] bg-gray-800 rounded my-6">
+      <p className="text-white bg-transparent font-semibold py-2 px-4 rounded text-center text-[20px]">Class</p>
+         <div className="h-20 w-40 rounded-full bg-orange-300 border-white flex justify-center items-center">
             <p className="text-white text-center self-center text-[30px]">
             {stat}</p>
          </div>
-         <div className="mb-20 pb-3">
-            <button className="absolute flex items-center justify-center mx-[48px] hover:text-gray-500 hover:rounded transform transition-all duration-300 hover:scale-110 pb-[25px]"
+         <div className="mb-20 py-[2px]">
+            <button className="absolute flex items-center justify-center mx-[48px] hover:text-orange-300 hover:rounded text-gray-800 transform transition-all duration-300 hover:scale-110"
             onClick={() => setStat(characterClass[Math.floor(Math.random() * characterClass.length)])}>
-               
-            <FaDiceOne size={64} />
+            <FaDiceD20 size={64} />
             </button>
          </div>
-      </div>   
-   </div>
-   // <div>ClassGenerator</div>
-    
-
+      </div>
+</div>
   )
 }
 
