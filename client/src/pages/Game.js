07aacc4d68b3965aa3317d5a957545d3cdd6411c
Jesus <perez.jesus1039@gmail.com>
2023-04-11 19:28:10 -0700

    added more to spells and added information to game.js

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,10 +1,15 @@
 const Game= () => {
   return (
   <div className="flex justify-center">
-    <h1 className="text-center">
-      Game coming soon...
+    <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+      Character Dress Up Game
     </h1>
+    <p className="mt-3 text-lg text-gray-500">
+      In this mini game you will be able to customize what your character will look like. Choose your class and race on the 
+      home page then mix and match charcter outfits.
+    </p>
     </div>
+
   );
 };
 
