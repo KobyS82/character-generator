966debf642d65b105c9b59a8f510e4fed58f88fb
Jesus <perez.jesus1039@gmail.com>
2023-04-11 17:19:36 -0700

    added a spells cheatsheet section using a dnd api

--- client/src/pages/Creators.js
+++ client/src/pages/Creators.js
@@ -1,6 +1,6 @@
 /*This is only a place holder for now. Replace this with the home page or something else */
 
-const Creators= () => {
+/*const Creators= () => {
     return (
     <div className="flex justify-center">
       <h1 className="text-center">
@@ -10,4 +10,32 @@ const Creators= () => {
     );
   };
 
-  export default Creators
+  export default Creators*/
+
+  import React, { useState, useEffect } from 'react';
+
+  function Creators() {
+    const [spells, setSpells] = useState([]);
+
+    useEffect(() => {
+      fetch('https://api.open5e.com/spells')
+      .then(response => response.json())
+      .then(data => setSpells(data.results))
+      .catch(error => console.error(error));
+    }, []);
+
+    return (
+      <div className='text-gray-500 py-4 text-center'>
+        <h1 className='text-3xl font-bold mb-4'>Spells</h1>
+        <h3 className='text-black'>Use this spells cheetsheet in your battles. Be smart and fight well! </h3>
+        <br></br>
+        <ul>
+          {spells.map(spell => (
+            <li key={spell.slig} className='text-black mb-2'>{spell.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  export default Creators 
