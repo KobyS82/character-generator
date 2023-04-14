244608e385fa372751df43a6c1c94e1c8f5a799f
Jungwoo Lee <jungwoo33@gmail.com>
2023-04-13 20:14:02 -0500

    logout button added

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Navbar = () => {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
     return (
         <nav className="bg-[#2d3e50] text-white px-10 py-10 mb-4 flex flex-col md:flex-row justify-between items-center w-[100vw]">
             <Link to="/">
@@ -11,9 +16,14 @@ const Navbar = () => {
                 <Link to="/spells" className="underline text-white hover:text-blue-600 mr-4">Spells</Link>
                 <Link to="/game" className="underline text-white hover:text-blue-600 mr-4">Mini Game</Link>
                 <Link to="/contact" className="underline text-white hover:text-blue-600 mr-4">Contact</Link>
+                {Auth.loggedIn() ? (
+                    <button className='btn btn-lg btn-light m-2' onClick={logout}>Logout</button>
+                ) : (
+                    <>
                 <Link to="/login" className="underline text-white hover:text-blue-600 mr-4">Login</Link>
                 <Link to="/register" className="underline text-white hover:text-blue-600">Register</Link>
-
+                </>
+                )}
             </div>
         </nav>
     )
