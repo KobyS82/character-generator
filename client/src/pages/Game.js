6ccb82f3d4f52f5f64a3347ab4f92fed148487b2
Jesus <perez.jesus1039@gmail.com>
2023-04-13 17:03:52 -0700

    adding to the mini game

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,13 +1,22 @@
 const Game= () => {
   return (
-  <div className="flex justify-center">
-    <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+  <div className="flex flex-col justify-center">
+    <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl text-center">
       Character Dress Up Game
     </h1>
-    <p className="mt-3 text-lg text-gray-500">
-      In this mini game you will be able to customize what your character will look like. Choose your class and race on the 
-      home page then mix and match character outfits.
-    </p>
+    <br></br>
+
+      <div className="text-center">
+        <h4 className="text-2xl">Be able to change your characters clothes and style using this outfit generator</h4>
+        <p className="font-bold">The process is simple!</p>
+        <br></br>
+        <ol className="text-center">
+        <li>Choose your class</li>
+        <li>Choose your race</li>
+        <li>Mix and match your characters styles</li>
+        <li>Have fun!</li>
+        </ol>
+      </div>
     </div>
 
   );
